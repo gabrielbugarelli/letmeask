987d63533256c93f943043b388876f28259e90f1
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-02-24 22:17:15 -0400

    feat: implementação do método para buscar sala.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,11 @@ import logoImg from '../assets/images/logo.png';
 import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss'
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebaseConnection';
 
 export const Home = () => {
+  const [ roomCode, setRoomCode ] = useState('');
 
   const navigate = useNavigate();
 
@@ -24,6 +27,23 @@ export const Home = () => {
     }
 
     navigate('/rooms/new');
+  }
+
+  /**
+   * @description o método valida se a sala existe, se existir, o usuário será redirecionado
+   * para a sala de acordo com o código digitado. 
+   */
+  const handleJoinRoom = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()) {
+      alert('Room does not exist.');
+      return;
+    }
+
+    navigate(`rooms/${roomCode}`);
   }
 
 	return (
@@ -49,10 +69,12 @@ export const Home = () => {
             ou entre na sala
           </div>
 
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text" 
               placeholder='Digite o código da sala'
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
 
             <Button type='submit'> Entrar na sala </Button>
