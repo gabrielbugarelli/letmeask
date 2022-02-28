130d20493d7c9cd555f93bd4af7be7aed014014b
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-02-28 16:45:04 -0400

    feat(room): integração com o firebase

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
@@ -8,16 +8,61 @@ import { database } from '../services/firebaseConnection';
 
 import '../styles/room.scss';
 
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string
+    avatar: string
+  }
+  content: string
+  isAnswered: boolean
+  isHighlighted: boolean
+}>
+
+type Question = {
+  id: string
+  author: {
+    name: string
+    avatar: string
+  }
+  content: string
+  isAnswered: boolean
+  isHighlighted: boolean
+}
+
 type RoomParams = {
   id: string;
 }
 
 export const Room = () => {
-  const [ newQuestion, setNewQuestion ] = useState('');
   const { user } = useAuth();
-
   const params = useParams<RoomParams>();
   const roomId = params.id ? params.id : '';
+
+  const [ newQuestion, setNewQuestion ] = useState('');
+  const [ questions, setQuestions ] = useState<Question[]>([]);
+  const [ title, setTitle ] = useState();
+
+  useEffect(() => {
+    const roomRef = database.ref(`/rooms/${roomId}`);
+
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isAnswered: value.isAnswered,
+          isHighlighted: value.isHighlighted
+        }
+      });
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions)
+    })
+  }, [roomId])
 
   const handleSendQuestion = async (event: FormEvent) => {
     event.preventDefault();
@@ -40,7 +85,6 @@ export const Room = () => {
       isAnswered: false
     }
 
-    
     await database.ref(`rooms/${roomId}/questions`).push(question);
     setNewQuestion('');
   }
@@ -56,8 +100,8 @@ export const Room = () => {
 
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas</span>
+          <h1>Sala: {title}</h1>
+          { questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
 
         <form onSubmit={handleSendQuestion}>
