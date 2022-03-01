a86891bb442eb589bffe8188fca0e2de55428afc
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-03-01 00:17:08 -0400

    refactor: style Room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,11 +1,12 @@
 import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebaseConnection';
+
+import logoImg from '../assets/images/logo.svg';
 
 import '../styles/room.scss';
 
